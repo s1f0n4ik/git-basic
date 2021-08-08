b4afccec828ed8c7f7594d01f1be90e2496f9bb9
s1f0n4ik <sifon8998@mail.ru>
2021-08-08 14:07:11 +0300

    add opportunity to change word for print

--- aboba.py
+++ aboba.py
@@ -1,7 +1,14 @@
 count = 0
+a = "aboba!"
 while True:
-    print("aboba!")
+    print(a)
     count += 1
     if count >= 100:
         print("Конец!")
         break
+    choice = input("Хотите поменять слово?")
+    if choice == "Да":
+        name = input("Введите ваше слово: ")
+        a = name
+    else:
+        continue

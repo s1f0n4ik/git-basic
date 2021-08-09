e0a99ca5682ea87906218ecafaeedebafcbb78da
s1f0n4ik <sifon8998@mail.ru>
2021-08-09 11:25:51 +0300

    'Tab' lines 15-24

--- aboba.py
+++ aboba.py
@@ -12,14 +12,14 @@ while True:
         a = name
     else:
         continue
-if count == 10:
-    cont = input("Продолжаем?(Да или Нет)")
-    if cont == "Нет":
-        print("Конец!")
-        break
-    elif cont == "Да":
-        continue
-    else:
-        print("Error!")
-        break
+    if count == 10:
+        cont = input("Продолжаем?(Да или Нет)")
+        if cont == "Нет":
+            print("Конец!")
+            break
+        elif cont == "Да":
+            continue
+        else:
+            print("Error!")
+            break
 print("aboba!")
